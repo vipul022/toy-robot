47b30482ac5babaab7bb2a9a648170623a05dc42
vipul022 <vipul.seth022@gmail.com>
2021-04-26 11:59:25 +1000

    created new project using create react-app and typescript and added basic form to place the robot on table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Toy Robot</h1>
       </header>
-    </div>
+      <h4>Place your robot on the table</h4>
+      <form>
+        <div>
+          <label htmlFor="x-coordinates">X-coordinates</label>
+          <input type="number" name="x-coordinates" id="x- coordinates"></input>
+        </div>
+        <div>
+          <label htmlFor="y-coordinates">Y-coordinates</label>
+          <input type="number" name="y-coordinates" id="y- coordinates"></input>
+        </div>
+        <div>
+          <label htmlFor="north">North</label>
+          <input type="radio" id="north" name="direction" value="north"></input>
+          <label htmlFor="east">East</label>
+          <input type="radio" id="east" name="direction" value="east"></input>
+          <label htmlFor="south">South</label>
+          <input type="radio" id="south" name="direction" value="south"></input>
+          <label htmlFor="west">West</label>
+          <input type="radio" id="west" name="direction" value="west"></input>
+        </div>
+        <input type="submit" value="Place"></input>
+      </form>
+    </>
   );
-}
+};
 
 export default App;
